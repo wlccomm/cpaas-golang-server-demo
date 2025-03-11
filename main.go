0114b32ef7cd1ae3d00641913c8a7560038af40f
Greg Lownes <glownes@whitelabelcomm.com>
2025-03-11 06:58:55 -0400

    add cors

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.Default())
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
